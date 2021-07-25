cf4bd430e6fcd9b30446fed8dc795a24c639a46e
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 14:27:17 -0500

    cambiar email com autentificador de django

--- my_blog/users/models.py
+++ my_blog/users/models.py
@@ -2,7 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     web_site = models.CharField(max_length=255, blank=True)
-    
+    twitter = models.CharField(max_length=255, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
