6ef9dfda4425ebf8d8c4f2061cfe437bc47095a5
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 14:14:18 -0500

    personalizar el model panel admin del usuario

--- my_blog/users/admin.py
+++ my_blog/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    
+    fieldsets = (
+        (None,{'fields':('username','password')}),
+        ('Informacion personal',{'fields':('first_name','last_name','email')}),
+        ('Permisos',{'fields':('is_active','is_staff','is_superuser','groups', 'user_permissions',)}),
+    )
