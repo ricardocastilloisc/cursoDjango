169976f3bff1eb963ecfca0e619f87a2e685371d
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 15:17:53 -0500

    base de datos posgress

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'DB',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
