5434c0a1e143819f596e2fa566a98df6ed8f5efc
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 12:55:03 -0500

    crud completo con pocas lineas

--- my_blog/posts/api/router.py
+++ my_blog/posts/api/router.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from posts.api.views import PostApiViewSet
+from posts.api.views import PostModelViewSet
 
 router_post = DefaultRouter()
 
-router_post.register(prefix='posts', basename='posts', viewset=PostApiViewSet)
+router_post.register(prefix='posts', basename='posts', viewset=PostModelViewSet)
--- my_blog/posts/api/views.py
+++ my_blog/posts/api/views.py
@@ -3,24 +3,29 @@ from rest_framework.response import Response
 from rest_framework import status
 from posts.models import  Post
 from posts.api.serializers import PostSerializer
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
-
-class PostApiViewSet(ViewSet):
-    def list(self, request):
-        #posts = Post.objects.all()
-        serializer = PostSerializer(Post.objects.all(), many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
-
-    def retrieve(self, request, pk: int):
-        post = PostSerializer(Post.objects.get(pk=pk))
-        return Response(status=status.HTTP_200_OK, data=post.data)
+class PostModelViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
     
-    def create(self, request):
-        serializer = PostSerializer(data=request.POST)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+#class PostApiViewSet(ViewSet):
+#    def list(self, request):
+#        #posts = Post.objects.all()
+#        serializer = PostSerializer(Post.objects.all(), many=True)
+#        return Response(status=status.HTTP_200_OK, data=serializer.data)
+#
+#    def retrieve(self, request, pk: int):
+#        post = PostSerializer(Post.objects.get(pk=pk))
+#        return Response(status=status.HTTP_200_OK, data=post.data)
+#    
+#    def create(self, request):
+#        serializer = PostSerializer(data=request.POST)
+#        serializer.is_valid(raise_exception=True)
+#        serializer.save()
+#        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
 #class PostApiView(APIView):
 #    def get(self, request):
