3d57e5d60ad67534765f4c70a1171a4838a3ffdd
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-07-25 12:47:33 -0500

    viewSet

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from posts.api.views import PostApiView
+from posts.api.router import router_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/post/', PostApiView.as_view())
+    path('api/', include(router_post.urls))
 ]
